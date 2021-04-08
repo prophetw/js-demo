7c2ec70497252034e63cc48383143f0486a641b9
root <root@DESKTOP-I2ET75H.localdomain>
2021-04-08 11:01:31 +0800

    feat: add print for priority

--- src/utils/priorityQueue.test.ts
+++ src/utils/priorityQueue.test.ts
@@ -43,7 +43,6 @@ class PriorityQ {
   }
   up(itemIndex: number) {
     // 上浮操作 新加入的元素
-
     // 停止条件
     // 1. 当前 item index 大于0
     // 2. 和上级比较 不需要交换
@@ -101,18 +100,76 @@ class PriorityQ {
     this.sink(0)
     return res
   }
-}
-const reorder = () => {
-  // 变成最大堆或者最小堆
-}
+  getLastRowWith() {
+    // 计算最后一行的宽度
+    return 10
+  }
 
-const swaped = (arr: any[], a: number, b: number) => {
-  // 数组内部元素交换
-  const tmp = arr[a]
-  arr[a] = arr[b]
-  arr[b] = tmp
+  print() {
+    // 打印出来 最后一排的宽度
+    function whiteSpace(num: number) {
+      let result = ''
+      while (num > 0) {
+        result += '-'
+        num = num - 1
+      }
+      return result
+    }
+    const lastRowNum = getLastRowNum(this.queue)
+    const lastRowWith = Math.pow(2, lastRowNum)
+    let str = ''
+    this.queue.map((item, index: number) => {
+      const curRowNum = getLastRowNum(new Array(index + 1)) // start from 0
+      const curRowWith = Math.pow(2, curRowNum)
+      const curRowFirstIndex = Math.pow(2, curRowNum) - 1
+      const curRowLastIndex = Math.pow(2, curRowNum + 1) - 2
+      const everyBlockWith = lastRowWith / curRowWith
+      if (index === curRowFirstIndex && index === curRowLastIndex) {
+        // 第一行特殊处理
+        str += whiteSpace(everyBlockWith)
+        str += item
+        str += whiteSpace(everyBlockWith - 1)
+        str += '\n'
+        return
+      }
+      if (index === curRowFirstIndex) {
+        str += whiteSpace(everyBlockWith)
+        str += item
+        str += whiteSpace(everyBlockWith * 2 - 1)
+        return
+      }
+      if (index === curRowLastIndex) {
+        str += item
+        str += whiteSpace(everyBlockWith - 1)
+        str += '\n'
+        return
+      }
+      if (curRowFirstIndex < index && index < curRowLastIndex) {
+        str += item
+        str += whiteSpace(everyBlockWith * 2 - 1)
+        return
+      }
+    })
+    console.log(str)
+  }
 }
-
+function getLastRowNum(arr) {
+  // row 是从第0行开始的
+  // 计算最后的 index 在第几行0
+  let res = 0
+  const lastIndex = arr.length - 1
+  let rowNum = 0
+  while (lastIndex > -1) {
+    let curRowFirstIndex = Math.pow(2, rowNum) - 1
+    let nextRowFirstIndex = Math.pow(2, rowNum + 1) - 1
+    if (lastIndex === curRowFirstIndex) return rowNum
+    if (curRowFirstIndex < lastIndex && lastIndex < nextRowFirstIndex) {
+      return rowNum
+    }
+    rowNum = rowNum + 1
+  }
+  return res
+}
 describe(' test queue ', () => {
   test(' test ', () => {
     const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6]
@@ -127,6 +184,16 @@ describe(' test queue ', () => {
     console.log(queue)
     const s = queue.getTopItem()
     console.log(queue)
+    queue.print()
     expect(1).toBe(1)
+  })
+  test(' getLastRowNum ', () => {
+    expect(getLastRowNum(new Array(6))).toBe(2)
+    expect(getLastRowNum(new Array(7))).toBe(2)
+    expect(getLastRowNum(new Array(8))).toBe(3)
+    expect(getLastRowNum(new Array(15))).toBe(3)
+    expect(getLastRowNum(new Array(16))).toBe(4)
+    expect(getLastRowNum(new Array(1))).toBe(0)
+    expect(getLastRowNum(new Array(2))).toBe(1)
   })
 })
